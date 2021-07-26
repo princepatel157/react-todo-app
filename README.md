b9b5eb760c14fab0192e40b6794ded066d3b9954
Prince Patel <69680860+princepatel157@users.noreply.github.com>
2021-07-26 23:35:17 +0530

    add and delete todo

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # TODO APP
 
-top elemment done
+### Top Header Element:
+
+_edit and add item-_
 
 ![header screenshot](ss/top.JPG)
+
+### Items in the List:
+
+_here we can check, edit and delete item-_
+
+![todo list](ss/todo.JPG)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import img from "./img/him.jpg";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+
+  const [item, setItem] = useState([]);
+
+  const addItem = () => {
+    setItem([...item, todo]);
+    setTodo("");
+  };
+
+  const deleteItem = (ind) => {
+    const updatedList = item.filter((item, i) => {
+      return ind != i;
+    });
+    setItem(updatedList);
+  };
+
   return (
     <>
-      {/* main body */}
       <div className="main-body">
         {/* bg img div */}
         <div
@@ -24,9 +39,36 @@ const App = () => {
               type="text"
               className="input-field"
               placeholder="Enter Todo"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
             />
-            <input type="button" className="add-btn" value="Add" />
+            <input
+              type="button"
+              onClick={addItem}
+              className="add-btn"
+              value="Add"
+            />
           </section>
+
+          {/* todo items */}
+          {item.map((element, index) => {
+            return (
+              <section className="todo-list">
+                <div type="text" className="list-item">
+                  {element}
+                </div>
+                <button className="edit-item">
+                  <i className="fas fa-edit"></i>
+                </button>
+                <button
+                  onClick={() => deleteItem(index)}
+                  className="delete-item"
+                >
+                  <i className="fas fa-trash"></i>
+                </button>
+              </section>
+            );
+          })}
         </div>
       </div>
     </>
