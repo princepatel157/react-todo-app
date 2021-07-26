fa0e24b5fa1e179793d58971fd0c2426b9ddd585
Prince Patel <69680860+princepatel157@users.noreply.github.com>
2021-07-26 18:36:08 +0530

    top work done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import "./App.css";
+import img from "./img/him.jpg";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <>
+      {/* main body */}
+      <div className="main-body">
+        {/* bg img div */}
+        <div
+          className="bg-body"
+          style={{ backgroundImage: `url(${img}) ` }}
+        ></div>
+
+        {/* main center list frame  */}
+        <div className="main-frame">
+          {/* main heading */}
+          <section className="main-heading">TODO LIST</section>
+
+          {/* edit add todo */}
+          <section className="add-todo">
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Todo"
+            />
+            <input type="button" className="add-btn" value="Add" />
+          </section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
